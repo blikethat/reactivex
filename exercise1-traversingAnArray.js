72a6eb6485dbafa6c62f16ead8dbc82c35d31aa4
blikethat <kamie.lyn@gmail.com>
2016-05-29 16:09:15 -0700

    initial commit completed reactivex exercies 1-9

---
+++ exercise1-traversingAnArray.js
@@ -0,0 +1,10 @@
+//http://reactivex.io/learnrx/
+//Exercise 1: Print all the names in an array
+function ex1 (value) {
+	var names = ["Ben", "Jafar", "Matt", "Priya", "Brian"],
+		counter;
+	for(counter = 0; counter < names.length; counter++) {
+		console.log(names[counter]);
+	}
+}
+ex1("console names please");
---
+++ exercise2-forEach.js
@@ -0,0 +1,10 @@
+//http://reactivex.io/learnrx/
+//Exercise 2: Use forEach to print all the names in an array
+function ex2(value) {
+	var names = ["Ben", "Jafar", "Matt", "Priya", "Brian"];
+
+	names.forEach(function(name) {
+		console.log(name);
+	});
+}
+ex2("using forEACH");
