57456697c825da61d9978f5b1dc7109046c232cf
blikethat <kamie.lyn@gmail.com>
2016-05-29 16:31:52 -0700

    refactored exercise 10 so it is more efficient

--- exercise10-concatAll.js
+++ exercise10-concatAll.js
@@ -1,16 +1,17 @@
 //Exercise 10: Implement concatAll()
 //Let's add a concatAll() function to the Array type. The concatAll() function iterates over each sub-array in the array and collects the results in a new, flat array. Notice that the concatAll() function expects each item in the array to be another array.
 
-Array.prototype.concatAll = function() {
-	var results = [];
-  this.forEach(function(subArray) {
-    for(var counter = 0; counter < subArray.length; counter++){
-      results.push(subArray[counter]);
-    }
-	});
+  Array.prototype.concatAll = function() {
+  	var results = [];
+    this.forEach(function(subArray) {
+      results.push.apply(results, subArray); //more efficient 
+      // for(var counter = 0; counter < subArray.length; counter++){
+      //   results.push(subArray[counter]);
+      // }
+  	});
 
-	return results;
-};
+  	return results;
+  };
 
-console.log(JSON.stringify([ [1,2,3], [4,5,6], [7,8,9] ].concatAll())); //=== "[1,2,3,4,5,6,7,8,9]"
+  console.log(JSON.stringify([ [1,2,3], [4,5,6], [7,8,9] ].concatAll())); //=== "[1,2,3,4,5,6,7,8,9]"
 // [1,2,3].concatAll(); // throws an error because this is a one-dimensional array
